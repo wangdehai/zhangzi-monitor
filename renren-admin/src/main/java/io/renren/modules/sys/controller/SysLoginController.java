6252c4f8548fa8edf6fd2b7522f308021a1f87b3
WDH <594340717@qq.com>
2020-10-19 15:58:13 +0800

    update

--- renren-admin/src/main/java/io/renren/modules/sys/controller/SysLoginController.java
+++ renren-admin/src/main/java/io/renren/modules/sys/controller/SysLoginController.java
@@ -106,7 +106,9 @@ public class SysLoginController extends AbstractController {
         } catch (AuthenticationException e) {
             return R.error("账户验证失败");
         }
-
+        if("1".equals(parkId)){
+            return R.ok().put("code",1);
+        }
         return R.ok();
     }
     /**
